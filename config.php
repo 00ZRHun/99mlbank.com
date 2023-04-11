08a55a496a8ad49845420eab712ac49a30601a5a
00ZRHun <00zrhun@gmail.com>
2023-04-12 02:27:21 +0800

    feat: adjust config

--- config.php
+++ config.php
@@ -1,4 +1,6 @@
 <?php
+/* if (!isset($_SESSION))
+  session_start(); */
 session_start();
 date_default_timezone_set("Asia/Kuala_Lumpur");
 //include('auth.php');
@@ -6,8 +8,11 @@ $servername = "localhost";
 /*$username = "ccrdskmy_loansw";
 $password = "gq5QBO,Z8UQN";
 $dbname = "ccrdskmy_loan_software";*/
-$username = "ecadmin_crm";
+/* $username = "ecadmin_crm";
 $password = "bXwM}E=ME^!t";
+$dbname = "ecadmin_crm"; */
+$username = "zrhun";
+$password = "password";
 $dbname = "ecadmin_crm";
 
 //define("SITEURL","http://readyforyourreview.com/Loan_software"); 
@@ -17,13 +22,21 @@ $conn = new mysqli($servername, $username, $password, $dbname);
 // Check connection
 if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
-} 
+} /* else {   // D
+  echo ("Connection successful");
+} */
 $host_name = $_SERVER['SERVER_NAME'];
-$server_ssl = $_SERVER['REQUEST_SCHEME'];
-$url = $server_ssl.'://'.$host_name;
+$host_name = "crm.ecomdemo.xyz";   // * TODO: 99mlbank.com
+/* $server_ssl = $_SERVER['REQUEST_SCHEME'];
+$url = $server_ssl.'://'.$host_name; */
+$url = "http://localhost:8080";   // OPT: http://crm.ecomdemo.xyz
+// echo '$url = ' . $url;
 // print_r($_SERVER);
 // define("SITEURL","http://crm.ecomdemo.xyz");
- define("SITEURL",$url);
+/* if (!defined("SITEURL"))
+  define("SITEURL", $url); */
+define("SITEURL", $url);
+// $_SESSION["SITEURL"] = $url;   // TODO: find a way to get SITEURL as global var / store as
 
 
 //$host_name = 
@@ -34,5 +47,3 @@ $result_superadmin = mysqli_fetch_assoc($row_superadmin);
 $domain_status = $result_superadmin['status'];
 $table_prefix = $result_superadmin['table_prefix'];
 $first_method = $result_superadmin['first_method'];
-
-?>
