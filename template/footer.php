4d5b70f4be263ae7d7d3692b53218efaae2240e9
00ZRHun <00zrhun@gmail.com>
2023-04-12 02:31:33 +0800

    feat: clone template for header & footer

---
+++ template/footer.php
@@ -0,0 +1,73 @@
+<?php
+/* echo '$url = ' . $url;
+$url = parse_url($url, PHP_URL_SCHEME) . '://' . parse_url($url, PHP_URL_HOST);
+echo '$url = ' . $url;
+$base_url = trim($url, '/');
+echo '$base_url = ' . $base_url; */
+// echo '$base_url = ' . $url;
+/* echo '$url = ' . $url;
+$base_url = "http://localhost:8080"; */
+// echo '$url = ' . $url;
+?>
+
+<!--footer-->
+<footer class="footer">
+    <div class="container">
+        <div class="row align-items-center flex-row-reverse">
+            <div class="col-md-12 col-sm-12 mt-3 mt-lg-0 text-center">
+                Copyright © 2022 <a>Card Management System</a>. All rights reserved.
+            </div>
+        </div>
+    </div>
+</footer>
+<!-- End Footer-->
+
+</div>
+
+<!-- Back to top -->
+<a href="#top" id="back-to-top"><i class="fa fa-rocket"></i></a>
+
+<!-- Dashboard Core -->
+<script src="<?= SITEURL ?>/assets/js/vendors/jquery-3.2.1.min.js"></script>
+<script src="<?= SITEURL ?>/assets/plugins/bootstrap-4.3.1-dist/js/popper.min.js"></script>
+<script src="<?= SITEURL ?>/assets/plugins/bootstrap-4.3.1-dist/js/bootstrap.min.js"></script>
+<script src="<?= SITEURL ?>/assets/js/vendors/jquery.sparkline.min.js"></script>
+<script src="<?= SITEURL ?>/assets/js/vendors/selectize.min.js"></script>
+<script src="<?= SITEURL ?>/assets/js/vendors/jquery.tablesorter.min.js"></script>
+<script src="<?= SITEURL ?>/assets/js/vendors/circle-progress.min.js"></script>
+<script src="<?= SITEURL ?>/assets/plugins/rating/jquery.rating-stars.js"></script>
+
+<!--Counters -->
+<script src="<?= SITEURL ?>/assets/plugins/counters/counterup.min.js"></script>
+<script src="<?= SITEURL ?>/assets/plugins/counters/waypoints.min.js"></script>
+<!-- Fullside-menu Js-->
+<script src="<?= SITEURL ?>/assets/plugins/toggle-sidebar/sidemenu.js"></script>
+
+
+<!-- Data tables -->
+<script src="<?= SITEURL ?>/assets/plugins/datatable/jquery.dataTables.min.js"></script>
+<script src="<?= SITEURL ?>/assets/plugins/datatable/dataTables.bootstrap4.min.js"></script>
+<script src="<?= SITEURL ?>/assets/js/datatable.js"></script>
+
+<!-- Select2 js -->
+<script src="<?= SITEURL ?>/assets/plugins/select2/select2.full.min.js"></script>
+
+<!-- CHARTJS CHART -->
+<script src="<?= SITEURL ?>/assets/plugins/chart/Chart.bundle.js"></script>
+<script src="<?= SITEURL ?>/assets/plugins/chart/utils.js"></script>
+
+<!-- p-scroll bar Js-->
+<script src="<?= SITEURL ?>/assets/plugins/pscrollbar/pscrollbar.js"></script>
+<script src="<?= SITEURL ?>/assets/plugins/pscrollbar/pscroll.js"></script>
+
+<!-- ECharts Plugin -->
+<script src="<?= SITEURL ?>/assets/plugins/echarts/echarts.js"></script>
+<script src="<?= SITEURL ?>/assets/js/index1.js"></script>
+
+<!-- Custom Js-->
+<script src="<?= SITEURL ?>/assets/js/admin-custom.js"></script>
+<script src="<?= SITEURL ?>/assets/js/select2.js"></script>
+
+</body>
+
+</html>
