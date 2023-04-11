f5d7e8e8b7ef897076a55fb595df9503180023b0
00ZRHun <00zrhun@gmail.com>
2023-04-12 02:27:46 +0800

    feat: add asset folder

---
+++ assets/js/datatable.js
@@ -0,0 +1,11 @@
+$(function(e) {
+	
+	$('#example').DataTable();
+	$('#example2').DataTable();
+	$('#example3').DataTable();
+	$('#example4').DataTable();
+	$('#example5').DataTable();
+	$('#example6').DataTable();
+	$('#example7').DataTable();
+	
+} );
