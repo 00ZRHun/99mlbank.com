0c3fa5f78041151011c7934d8e9f38ebcbee4c07
00ZRHun <00zrhun@gmail.com>
2023-04-13 23:01:59 +0800

    feat: add login function

--- index.php
+++ index.php
@@ -1,23 +1,38 @@
 <?php
-
 require_once('config.php');
-
-$error_msg = '';
 
 if (isset($_POST['login'])) {
     $username = $_POST['username'];
     $password = $_POST['password'];
-    echo '$username = ' . $username;
-    echo '$password = ' . $password;
+    /* echo '$username = ' . $username;
+    echo '$password = ' . $password; */
 
-    // TODO: chk if password is matched in database record
-    // $error_msg = 'Wrong password. Try Again.';
+    // chk if password is matched in database record
+    $get_admin_data = mysqli_query($conn, "SELECT * FROM administration where username = '$username' AND password = '$password'");
 
+    if (mysqli_num_rows($get_admin_data) > 0) {
+        $rowadmin = mysqli_fetch_assoc($get_admin_data);
+
+        // set SESSION
+        // $_SESSION["group_name"] = 'superadmin';
+        // $_SESSION["user_id"] = $rowadmin['id'];
+        $_SESSION["upline"] = $rowadmin['id'];
+
+        // TODO: get last login
+        // $sql_hwe = "select * from " . $table_prefix . "recently_login order by date desc LIMIT 0,1";
+
+        // $results_hwe = mysqli_query($conn, $sql_hwe);
+
+        // if (mysqli_num_rows($results_hwe) > 0) {
+        // 	$row_login = mysqli_fetch_assoc($results_hwe);
+        // 	$_SESSION['last_login'] = $row_login['user_id'];
+        // }
 ?>
-    <script>
-        window.location.replace("<?php echo SITEURL ?>/home.php");
-    </script>
+        <script>
+            window.location.replace("<?php echo SITEURL ?>/home.php");
+        </script>
 <?php
+    }
 }
 ?>
 
@@ -63,8 +78,6 @@ if (isset($_POST['login'])) {
 </head>
 
 <body class="construction-image h-100">
-
-    <?= $error_msg ?>
 
     <!--Loader-->
     <div id="global-loader">
