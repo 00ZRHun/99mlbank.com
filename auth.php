1f688e2321e236509b270622df3fd56e7f878264
zuronghun <128782538+zuronghun@users.noreply.github.com>
2023-04-18 23:40:14 +0800

    fix: fix pages (got layer/folder) are not logout correctly

--- auth.php
+++ auth.php
@@ -1,5 +1,6 @@
 <?php
 if (!(isset($_SESSION["user_id"]) && !empty($_SESSION["user_id"]))) {
-    echo "<script>window.location.href='index.php';</script>";
+    // echo "<script>window.location.href='" . SITEURL . "/index.php';</script>";
+    echo "<script>window.location.href='$url/index.php';</script>";   // ditto
     exit;
 }
