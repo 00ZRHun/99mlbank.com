23a26c352036d362fab9d5b42ab77f29a3cdaf5c
zuronghun <128782538+zuronghun@users.noreply.github.com>
2023-05-12 02:18:26 +0800

    chore: add TODO: add control access level

--- auth.php
+++ auth.php
@@ -1,4 +1,5 @@
 <?php
+// TODO: add control access level
 if (!(isset($_SESSION["user_id"]) && !empty($_SESSION["user_id"]))) {
     // echo "<script>window.location.href='" . SITEURL . "/index.php';</script>";
     echo "<script>window.location.href='$url/index.php';</script>";   // ditto
